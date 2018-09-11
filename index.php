fa5b71ad6cf4fe3c0f4615de30215d4f2a506bc0
Mikk Kelder <MikkKelder@users.noreply.github.com>
2018-09-11 14:22:21 +0300

    Lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <title>Mikk Kelder, Õppetöö</title>
+    </head>
+<body>
+    <h1>Mikk Kelder</h1>
+    <p>Siin on minu <a href="http://www.tlu.ee">TLÜ</a> õppetöö raames valminud leht ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+    <p>Kodune ülesanne on edukalt sooritatud1</p>
+    <img src="https://www.pets4homes.co.uk/images/breeds/56/large/9b8f8158056fc5420a03372c9772678e.jpeg" alt="kutsu" img {
+    border-radius: 50%;
+}>
+    <p>Minu sõber teeb ka <a href="../../~andrmal" target="_blank">veebi</a></p>
+    </body>
+</html>
